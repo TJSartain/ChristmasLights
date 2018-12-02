6ce2d4becef55e62c4accc6a148a072070ddecb3
TJ Sartain <tj@iTrinity-Inc.com>
2018-12-02 17:40:57 -0500

    Initial Commit

---
+++ ChristmasLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChristmasLights
+//
+//  Created by TJ Sartain on 12/2/18.
+//  Copyright © 2018 iTrinity Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
